6272c4e2b15853256c0af0d6d44d4abb90614b29
Aman sharma <amansharma303108@gmail.com>
2025-06-01 21:58:45 +0530

    Create Greetings.py

---
+++ Greetings.py
@@ -0,0 +1,6 @@
+#A python program that takes a user's name and age, then prints a greeting.
+name=str(input("Please Enter Your Name:"))
+age=int(input("Please Enter Your Age(in numbers):"))
+print(f"WELCOME {name} You Are Of {age} Years..!! ")
+print(f"Hello {name} you are {age} Years old..!!😊")
+#sucess
