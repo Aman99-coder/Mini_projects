60dba8766fc1313adaa4e5ea1c691e53580c803f
Aman sharma <amansharma303108@gmail.com>
2025-06-01 21:55:45 +0530

    Create greatest_of_four_number.py

---
+++ greatest_of_four_number.py
@@ -0,0 +1,17 @@
+# A python program to find greatest of four numbers entered by user.
+
+num1=input("Enter no.1:")
+num2=input("Enter no.2:")
+num3=input("Enter no.3:")
+num4=input("Enter no.4:")
+greatest=num1
+if num2>greatest:
+   greatest=num2
+if num3>greatest:
+    greatest=num3
+if num4>greatest:
+    greatest=num4
+print("greatest number is:",greatest)
+    
+
+#succed with gpt
