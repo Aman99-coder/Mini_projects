15a737648d99550db51b9d0cb8ced68aaca02b0e
Aman sharma <amansharma303108@gmail.com>
2025-06-08 18:11:26 +0530

    Create Num_polarity_checker.py

---
+++ Num_polarity_checker.py
@@ -0,0 +1,8 @@
+#  To Check if a number is positive, negative, or zero
+a=int(input('Enter a no:'))
+if a>0:
+    print('Number is Positive..')
+elif a<0:
+    print('Number is Negative..')
+else:
+    print('Number is Neutral or Zero..')
